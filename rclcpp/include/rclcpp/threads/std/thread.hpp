07551e6cf227f510de6201d90776bfb8a0d7151c
s-morita <32567894+smorita-esol@users.noreply.github.com>
2023-03-09 13:15:21 +0900

    feat: added rclcpp::thread implementation based on std::tread as a fallback.
    For the environments where an environment-specific implementation is not prepared yet, we also provide a fallback implementation of rclcpp::thread. With this, the modified Multi-thread executor can be used as the existing one in such an environment.

--- rclcpp/include/rclcpp/threads/std/thread.hpp
+++ rclcpp/include/rclcpp/threads/std/thread.hpp
@@ -15,8 +15,103 @@
 #ifndef RCLCPP__THREADS__STD__THREAD_HPP_
 #define RCLCPP__THREADS__STD__THREAD_HPP_
 
-// Not implemented so far.
-// The windows specific code will be implemented
-// while discussing the scheduling parameter passing feature at Real-time WG.
+#include <unistd.h>
+#include <thread>
+#include <condition_variable>
+#include <cstdio>
+#include <memory>
+#include <mutex>
+#include <system_error>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+
+#include "rclcpp/threads/std/thread_attribute.hpp"
+#include "rclcpp/visibility_control.hpp"
+
+namespace rclcpp
+{
+
+struct Thread
+{
+  using NativeHandleType = std::thread::native_handle_type;
+  using Attribute = detail::ThreadAttribute;
+  using Id = std::thread::id;
+
+  Thread() noexcept
+  : thread_{} {}
+  Thread(Thread &&) = default;
+  template<typename F, typename ... Args,
+    typename = std::enable_if_t<!std::is_same<std::decay_t<F>, Attribute>::value>>
+  explicit Thread(F && f, Args && ... args)
+  : thread_(std::forward<F>(f), std::forward<Args>(args)...)
+  {}
+  template<typename F, typename ... Args>
+  Thread(Attribute & attr, F && f, Args && ... args)
+  {
+    if (attr.set_value) {
+      throw std::runtime_error("std::thread can't set thread attribute");
+    }
+    thread_ = std::thread(std::forward<F>(f), std::forward<Args>(args)...);
+  }
+  Thread(Thread const &) = delete;
+  ~Thread() {}
+
+  Thread & operator=(Thread && other) noexcept = default;
+  Thread & operator=(Thread const &) = delete;
+
+  void swap(Thread & other)
+  {
+    using std::swap;
+    swap(thread_, other.thread_);
+  }
+
+  void join()
+  {
+    thread_.join();
+  }
+
+  bool joinable() const noexcept
+  {
+    return thread_.joinable();
+  }
+
+  void detach()
+  {
+    thread_.detach();
+  }
+
+  NativeHandleType native_handle()
+  {
+    return thread_.native_handle();
+  }
+
+  Id get_id() const noexcept
+  {
+    return thread_.get_id();
+  }
+
+  static int hardware_concurrency() noexcept
+  {
+    return std::thread::hardware_concurrency();
+  }
+
+private:
+  std::thread thread_;
+};
+
+inline void swap(Thread & t1, Thread & t2)
+{
+  t1.swap(t2);
+}
+
+namespace this_thread
+{
+
+using std::this_thread::get_id;
+
+}  // namespace this_thread
+
+}  // namespace rclcpp
 
 #endif  // RCLCPP__THREADS__STD__THREAD_HPP_
